6028a513e419150af193eac8e9cbae61d6f70fc1
Redwan Ahmed <redwanahmed1@hotmail.co.uk>
2021-03-23 16:48:42 +0000

    Added Axios package and connected Unsplash AI.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
 import React from "react";
+import axios from 'axios';
 import SearchBar from "./SearchBar";
 
-const App = () => {
-    return (
-    <div className="ui container" style={{ marginTop: '10px' }}>
-        <SearchBar />
-    </div>
-    );
+class App extends React.Component {
+
+    onSearchSubmit(term) {
+        //console.log(term);
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query : term },
+            headers: { Authorization: 'Client-ID MUumdS-0ueZ3QEueosjplUDEkf7hXkxx1UNH-bQn1Z8' }
+        }).then(response => {
+            // we log what object is being displayed
+            console.log(response.data.results);
+        });
+    }
+
+    render() {
+        return (
+        <div className="ui container" style={{ marginTop: '10px' }}>
+            <SearchBar onSubmit={this.onSearchSubmit} />
+        </div>
+        );
+    }
 }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,17 +2,26 @@ import React from "react";
 
 class SearchBar extends React.Component {
 
-    onInputChange(event) {
-        console.log(event.target.value);
-    }
+    state = { term: '' };
+
+    onFormSubmit = (event) => {
+// we want to stop the default action which submits data and refreshes the screen when we press enter.
+        event.preventDefault();
+
+//passing in the state term into the props so the parent component (app.js) can invoke the component and get the state.
+        this.props.onSubmit(this.state.term);
+    };
 
     render() {
         return (
         <div className="ui segment">
-            <form className="ui form">
+            <form className="ui form" onSubmit={this.onFormSubmit}>
                 <div className="field">
                     <label>Image Search</label>
-                <input type="text" onChange={this.onInputChange}/>
+                    <input 
+                    type="text" 
+                    value={this.state.term} 
+                    onChange={(e) => this.setState({ term: e.target.value })}/>
                 </div>
             </form>
         </div>
